d41cc25f7c9b8280ccf1e3d7d915e89848cdb4cf
magacholuizdev <magacholuiz@gmail.com>
2023-08-28 11:26:46 -0300

    Updated Code

---
+++ .next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST="{\"../node_modules/@azure/msal-browser/dist/controllers/ControllerFactory.mjs -> ./StandardController.mjs\":{\"id\":\"../node_modules/@azure/msal-browser/dist/controllers/ControllerFactory.mjs -> ./StandardController.mjs\",\"files\":[]}}"
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,10 @@ import Head from "next/head";
 import type { AppProps } from "next/app";
 import { DepartmentContextProvider } from "../src/context/DepartmentContext";
 import "../styles/globals.css";
-import { PageWithLayout } from "../types/PageWithLayout";
 import { MsalProvider } from "@azure/msal-react";
 import { PublicClientApplication } from "@azure/msal-browser";
 import { msalConfig } from "components/MicrosoftSignIn/authConfig";
+import { PageWithLayout } from "../types/PageWithLayout";
 
 interface MyAppProps extends AppProps {
   Component: PageWithLayout;
