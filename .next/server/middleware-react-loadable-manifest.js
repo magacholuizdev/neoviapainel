06a8d6c30776f4459c271a33e09aec245013829d
Luiz Fernando Magacho <luizmagacho94@gmail.com>
2023-10-15 12:45:04 -0300

    Delete .next directory

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"../../node_modules/@azure/msal-browser/dist/controllers/ControllerFactory.mjs -> ./StandardController.mjs\":{\"id\":\"../../node_modules/@azure/msal-browser/dist/controllers/ControllerFactory.mjs -> ./StandardController.mjs\",\"files\":[]}}"
